ca075052407cb71c53cdddfa111bb24e8ac0c43e
andresfvargasl94 <andresfvargasl94@gmail.com>
2015-05-17 22:01:21 +0000

    Actualizacion Servicios Rest, Se añade el registrar lector.

--- LookDocs/src/main/java/co/edu/usbcali/lookdocs/controller/ServiciosRESTLookDocs.java
+++ LookDocs/src/main/java/co/edu/usbcali/lookdocs/controller/ServiciosRESTLookDocs.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import co.edu.usbcali.lookdocs.model.Usuarios;
 import co.edu.usbcali.lookdocs.presentation.businessDelegate.IBusinessDelegatorView;
 
 @Controller
@@ -26,6 +27,26 @@ public class ServiciosRESTLookDocs {
 		try {
 			retorno=businessDelegatorView.iniciarSesionLector(name, pass);
 			return retorno;
+		} catch (Exception e) {
+			return e.getMessage();
+		}
+		
+	}
+	
+	@RequestMapping(value="/registrar",method=RequestMethod.POST)
+	@ResponseBody
+	public String registrar(String nombre, String correo, String pass, String passConfirma){
+		Usuarios usuario = new Usuarios();
+		try {
+			if(pass.trim().equals(passConfirma.trim())){
+			usuario.setNombre(nombre);
+			usuario.setEmail(correo);
+			usuario.setClave(passConfirma);
+			businessDelegatorView.registrarUsuarioLector(usuario);
+			return "exito";
+			}else{
+				return "Las contraseñas no coinciden";
+			}
 		} catch (Exception e) {
 			return e.getMessage();
 		}
